1e96447620a42998fa17ee40f82fd90ed0fd0ef1
신일환 <acoross@sin-ilhwan-ui-macbook-pro.local>
2016-06-23 22:16:31 +0900

    Initial Commit

---
+++ algorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  algorithm
+//
+//  Created by 신일환 on 2016. 6. 23..
+//  Copyright © 2016년 acoross. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
